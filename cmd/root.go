8f0182bf9e5d90f9fa588259bf8cc29f6c65d2bb
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 18:40:27 +0900

    config: add cobra and viper dep

---
+++ cmd/root.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "EssayGenie",
+	Short: "EssayGenie is a web application that writes a essay in English by just putting keyword in Korean",
+	Long:  "EssayGenie is a web application that writes a essay in English by just putting keyword in Korean.",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
