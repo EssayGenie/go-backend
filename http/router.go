297ed1c94c30428972642277c550232ac8193ad1
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 16:04:17 +0900

    config: add New Chi Router configuration

--- http/router.go
+++ http/router.go
@@ -1,1 +1,21 @@
 package http
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+type Router struct {
+	chi chi.Router
+}
+
+func NewRouter() *Router {
+	return &Router{
+		chi: chi.NewRouter(),
+	}
+}
+
+func (r *Router) Route(pattern string, fn func(*Router)) {
+	r.chi.Route(pattern, func(r chi.Router) {
+		fn(&Router{chi: r})
+	})
+}
