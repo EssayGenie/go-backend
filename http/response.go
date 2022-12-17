6efa4431b90579f9dbf8a526603b66e0192504a8
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 15:04:00 +0900

    feat: add SendJSON feature

--- http/response.go
+++ http/response.go
@@ -1,1 +1,22 @@
 package http
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+func SendJSON(w http.ResponseWriter, statusCode int, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	b, err := json.Marshal(data)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("Error marshalling JSON: %v", err))
+	}
+	_, err = w.Write(b)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("Error writing JSON: %v", err))
+	}
+	return nil
+}
