8633bee33734639bf00bb85c5766dc138d70a793
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 16:23:44 +0900

    config: add chi.Router configuration - please note that the `r.Use` and `r.With` is different

--- http/router.go
+++ http/router.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"github.com/go-chi/chi/v5"
+	"go-backend/http/handler"
+	"net/http"
 )
 
 type Router struct {
@@ -15,7 +17,44 @@ func NewRouter() *Router {
 }
 
 func (r *Router) Route(pattern string, fn func(*Router)) {
-	r.chi.Route(pattern, func(r chi.Router) {
-		fn(&Router{chi: r})
+	r.chi.Route(pattern, func(c chi.Router) {
+		fn(&Router{chi: c})
 	})
+}
+
+func (r *Router) Get(pattern string, fn handler.ApiHandler) {
+	r.chi.Get(pattern, handler.Handler(fn))
+}
+
+func (r *Router) Post(pattern string, fn handler.ApiHandler) {
+	r.chi.Post(pattern, handler.Handler(fn))
+}
+func (r *Router) Put(pattern string, fn handler.ApiHandler) {
+	r.chi.Put(pattern, handler.Handler(fn))
+}
+
+func (r *Router) Delete(pattern string, fn handler.ApiHandler) {
+	r.chi.Delete(pattern, handler.Handler(fn))
+}
+
+func (r *Router) With(fn handler.MiddlewareHandler) *Router {
+	c := r.chi.With(handler.GetMiddleware(fn))
+	return &Router{chi: c}
+}
+
+func (r *Router) WithBypass(fn func(next http.Handler) http.Handler) *Router {
+	c := r.chi.With(fn)
+	return &Router{chi: c}
+}
+
+func (r *Router) Use(fn handler.MiddlewareHandler) {
+	r.chi.Use(handler.GetMiddleware(fn))
+}
+
+func (r *Router) UseBypass(fn func(next http.Handler) http.Handler) {
+	r.chi.Use(fn)
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	r.chi.ServeHTTP(w, req)
 }
