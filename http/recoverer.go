761a9c0a2040db8aa12961223602711dd02de371
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 14:58:52 +0900

    config: add handleError feature to handle fatal error

--- http/recoverer.go
+++ http/recoverer.go
@@ -7,15 +7,15 @@ import (
 	"runtime/debug"
 )
 
-// Recoverer is a middleware that recovers from panics, logs the panic (and a
-// backtrace), and returns a HTTP 500 (Internal Server Error) status if
-// possible. Recoverer prints a request ID if one is provided.
+// Recoverer is a middleware that recovers from panics, logs the panic (and a backtrace)
+// then returns a HTTP 500 (Internal Server HTTPError) status if possible.
+// Recoverer prints a request ID if one is provided.
 func Recoverer(w http.ResponseWriter, r *http.Request) (context.Context, error) {
 	defer func() {
 		if healer := recover(); healer != nil {
 			log.Printf("PANIC: %v", debug.Stack())
 
-			he := &Error{
+			he := &HTTPError{
 				Code:    http.StatusInternalServerError,
 				Message: http.StatusText(http.StatusInternalServerError),
 			}
@@ -27,6 +27,22 @@ func Recoverer(w http.ResponseWriter, r *http.Request) (context.Context, error) 
 	return nil, nil
 }
 
-func handleError(err *Error, w http.ResponseWriter, r *http.Request) {
-	// write more...
+func handleError(err error, w http.ResponseWriter, r *http.Request) {
+	errorId := GetRequestId(r.Context())
+	switch e := err.(type) {
+	case *HTTPError:
+		if e.Code >= http.StatusInternalServerError {
+			log.Printf("HTTPError: %s", e.InternalError)
+			e.ErrorID = errorId
+			log.Println("ERROR :", e.Cause())
+		} else {
+			log.Println("INFO :", e.Cause())
+		}
+	default:
+		log.Println("UNHANDLED FATAL ERROR :", e)
+		w.WriteHeader(http.StatusInternalServerError)
+		if _, writeErr := w.Write([]byte(http.StatusText(http.StatusInternalServerError))); writeErr != nil {
+			log.Println("ERROR WRITING GENERIC ERROR MESSAGE :", err)
+		}
+	}
 }
