3b5bfcac57f7bbfa89fc672537e8aaf7e78d11d3
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 19:21:21 +0900

    config: logging file location

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"go-backend/cmd"
+	"log"
+	"os"
 )
 
 func main() {
+	// save here in the directory as logfile
+	logFile, err := os.OpenFile("logfile", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Unable to set logfile:", err.Error())
+	}
+	log.SetOutput(logFile)
 	cmd.Execute()
 }
