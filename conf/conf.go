fc3a8c2d9af1488f9c769e7d2c604efad112b390
Sigrid <sigrid.jinhyung@gmail.com>
2022-12-17 19:16:05 +0900

    refactor: conf to load global configuration - minor changes: endpoint

--- conf/conf.go
+++ conf/conf.go
@@ -1,9 +1,29 @@
 package conf
 
-import "time"
-
-type Configuration struct{}
+import (
+	"github.com/spf13/viper"
+	"time"
+)
 
 const (
 	TIMEOUT_IN_MINUTE = time.Minute
 )
+
+type Configuration struct{}
+
+type GlobalConfiguration struct {
+	EndPoint EndPoint `mapstructure:"endpoint"`
+}
+
+type EndPoint struct {
+	Host string `mapstructure:"host"`
+	Port int    `mapstructure:"port"`
+}
+
+func LoadGlobalConfig() (*GlobalConfiguration, error) {
+	var gconf GlobalConfiguration
+	if err := viper.Unmarshal(&gconf); err != nil {
+		return nil, err
+	}
+	return &gconf, nil
+}
